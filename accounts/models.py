e0019ef77a20502cf5a44e345f79481ecc8f9621
sarvarsssss <88589827+sarvarsssss@users.noreply.github.com>
2021-09-09 05:12:58 -0700

    Initial commit

---
+++ accounts/models.py
@@ -0,0 +1,23 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profiles',default='profiles/default.jpg')
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
